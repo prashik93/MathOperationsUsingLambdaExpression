d6caf75cb51e479c69f0e854bcfa7ef44b0dca4f
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-18 11:33:32 +0530

    MODIFIED : Code to pass lambda function to show result.

--- src/main/java/com/mathoperationusinglambdaexpression/MathOperationApp.java
+++ src/main/java/com/mathoperationusinglambdaexpression/MathOperationApp.java
@@ -3,6 +3,9 @@ package com.mathoperationusinglambdaexpression;
 @FunctionalInterface
 interface IMathFunction {
     int calculate(int a, int b);
+    static void printResult(int a, int b, String function, IMathFunction fobj) {
+        System.out.println("Result of " + function + " is " + fobj.calculate(a, b));
+    }
 }
 
 public class MathOperationApp {
@@ -12,9 +15,9 @@ public class MathOperationApp {
         IMathFunction multiply = (a , b) -> (a * b);
         IMathFunction divide = (int a, int b) -> (a / b);
 
-        System.out.println("Addition is " + add.calculate(12, 15));
-        System.out.println("Substraction is " + substract.calculate(25, 21));
-        System.out.println("Multiplication is " + multiply.calculate(2, 8));
-        System.out.println("Division is " + divide.calculate(24, 3));
+        IMathFunction.printResult(12, 15, "Addition", add);
+        IMathFunction.printResult(25, 21, "Substraction", substract);
+        IMathFunction.printResult(2, 8, "Multiplication", multiply);
+        IMathFunction.printResult(24, 8, "Division", divide);
     }
 }
