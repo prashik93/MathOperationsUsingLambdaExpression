169f63384fac032da290b21e76414edc7d974cec
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-18 12:31:38 +0530

    ADDED : Code to print double value using functional interface.

--- src/main/java/com/mathoperationusinglambdaexpression/NumberPlayList.java
+++ src/main/java/com/mathoperationusinglambdaexpression/NumberPlayList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NumberPlayList {
     public static void main(String[] args) {
@@ -42,6 +43,12 @@ public class NumberPlayList {
         //Method 5: Implicit Lambda Function
         myNumberList.forEach(n -> {
             System.out.println("Mth5 : forEach Lambda impl Value :: " + n);
+        });
+
+        //Method 6: Implicit Lambda Function to print double value
+        Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
+        myNumberList.forEach(n -> {
+            System.out.println("Mth6 : forEach Lambda double Value :: " + toDoubleFunction.apply(n));
         });
     }
 }
